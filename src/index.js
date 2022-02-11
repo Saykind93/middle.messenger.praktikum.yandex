d2443b825f58a67dc2abc61d02a6b9c14bbdbc96
Dima <saykind93@mail.ru>
2022-02-11 16:23:33 +0300

    new changes for netlify

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
    
    
    // compile the template
-    const Handlebars = require('handlebars')
-    var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
-    // execute the compiled template and print the output to the console
-    var filled = template({ doesWhat: "my hb!" })
+    // const Handlebars = require('handlebars')
+    // var template = Handlebars.compile("Handlebars <b>{{doesWhat}}</b>");
+    // // execute the compiled template and print the output to the console
+    // var filled = template({ doesWhat: "my hb!" })
 
- document.getElementById('root').innerHTML = filled
+ const root = document.getElementById('root')
+ root.textContent = "Мой проект спринт 1"
 
