aa42eb255b57f2a5243de1c13ffa9cd103e192e5
Dima <saykind93@mail.ru>
2022-02-14 16:40:57 +0300

    add scss files and change colors name

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const express = require("express");
 
 const app = express();
 app.use(express.static(__dirname + "/dist"));
-
 app.listen(process.env.PORT || 3000, function () {
   console.log(
     `Мой сервер на порт номер: ${process.env.PORT || 3000}`,
     __dirname
   );
 });
-
